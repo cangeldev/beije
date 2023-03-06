7a252517646d287feefc19749937e1d0b0a78221
Can <can.gel@hotmail.com>
2023-03-06 18:59:39 +0300

    Redux-toolkit eklendi.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Container } from './container'
+import { Provider } from 'react-redux'
+import { store } from './features/store'
 
 export default function App() {
   return (
-    <Container />
+    <Provider store={store}>
+      <Container />
+    </Provider>
   )
 }
