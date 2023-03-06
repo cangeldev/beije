3586fe19d728bec4b74cad4a0d6cefc66b100708
Can <can.gel@hotmail.com>
2023-03-06 18:40:45 +0300

    İlk sayfa ayarlamaları yapıldı.

---
+++ src/container/container.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { CustomPacketScreen } from '../customPacketScreen'
+
+export const Container = () => {
+    return (
+        <View>
+            <CustomPacketScreen />
+        </View>
+    )
+}
---
+++ src/customPacketScreen/customPacketScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const CustomPacketScreen = () => {
+    return (
+        <View>
+            <Text>customPacketScreen</Text>
+        </View>
+    )
+}
