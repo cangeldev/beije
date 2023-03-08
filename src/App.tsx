e29148cb007799360c66ad5b4ebd457f617127a6
Can <can.gel@hotmail.com>
2023-03-08 14:53:32 +0300

    Update.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 import { Container } from './container'
 import { Provider } from 'react-redux'
 import { store } from './features/store'
-
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...'])
+LogBox.ignoreAllLogs();
 export default function App() {
   return (
     <Provider store={store}>
